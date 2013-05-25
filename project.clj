768a442e4a1ebcc4b91aac112e9bb3c463672478
Martin Trojer <martin.trojer@gmail.com>
2013-05-25 20:32:23 +0100

    clojail

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [ring/ring-jetty-adapter "1.1.8"]
                  [ring/ring-json "0.2.0"]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.3"]]
+                 [hiccup "1.0.3"]
+                 [clojail "1.0.6"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler cljmirror.handler/site}
   :repl-options {:init-ns cljmirror.app
