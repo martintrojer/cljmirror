43a337ae7a286ab59aa03b31fbc4ee416a6f2a4d
Martin Trojer <martin.trojer@gmail.com>
2013-05-26 14:14:16 +0100

    better handling of eval results

--- resources/public/js/cljmirror.js
+++ resources/public/js/cljmirror.js
@@ -1,34 +1,58 @@
-$(document).ready(
-	function() {
-		var widgets = []
-		var cm = CodeMirror(
-			document.getElementById('console'),
-			{value :
-			 ";; Ctrl-Enter to evaluate\n\n"+
-			 "(defn fact [n]\n"+
-			 "  (if (zero? n) 1\n"+
-			 "    (* n (fact (dec n)))))\n",
-			 mode: "clojure",
-			 lineNumbers: true,
-			 autoCloseBrackets: true,
-			 matchBrackets: true,
-			 extraKeys:
-			 {"Ctrl-Enter":
-			  function() {
-				  var code = "(do " + cm.getValue() + ")";
-				  $.post("/eval", {code: code},
-							function(data, status) {
-								// TODO check status
-								var msg = document.createElement("div");
-								msg.className = "eval-result";
-								msg.appendChild(document.createTextNode(" ;; " + data.result));
-								var line = cm.getCursor().line
-								var cnt = cm.lineCount()-1
-								if (cnt <= line) {
-									cm.setValue(cm.getValue()+"\n");
-								}
-								cm.addLineWidget(line, msg, {});
-								cm.setCursor({line: line+1});
-							});}
-			 }})
-	});
+var widgets = [];
+
+function removeWidgets(line) {
+	while(widgets.length > 0) {
+		var w = widgets[widgets.length - 1];
+		if (w[0] >= line) {
+			w[1].clear();
+			widgets.pop();
+		} else break;
+	}
+}
+
+function evalResultFn(cm, line) {
+	return function(data, status) {
+		// TODO check status
+		var msg = document.createElement("div");
+		msg.className = "eval-result";
+		msg.appendChild(document.createTextNode(" ;; " + data.result));
+		var cnt = cm.lineCount()-1
+		if (cnt <= line) cm.setValue(cm.getValue()+"\n");
+		widgets.push([line, cm.addLineWidget(line, msg, {})]);
+		widgets.sort(function(a,b) {a[0] - b[0]});
+		cm.setCursor({line: line+1});
+		}
+}
+
+function doEvalFn(cm) {
+	return function() {
+		var line = cm.getCursor().line
+		var top = 0
+		removeWidgets(line);
+		if (widgets.length > 0)
+			top = widgets[widgets.length - 1][0]
+
+		var code = "(do " + cm.getRange({line: top}, {line: line}) + ")";
+
+		$.post("/eval", {code: code}, evalResultFn(cm, line));
+	}
+}
+
+// ---
+
+$(document).ready(function() {
+	var widgets = []
+	var cm = CodeMirror(
+		document.getElementById('console'),
+		{value :
+		 ";; Ctrl-Enter to evaluate\n\n"+
+		 "(defn fact [n]\n"+
+		 "  (if (zero? n) 1\n"+
+		 "    (* n (fact (dec n)))))\n"+
+		 "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+		 mode: "clojure",
+		 lineNumbers: true,
+		 autoCloseBrackets: true,
+		 matchBrackets: true});
+	cm.addKeyMap({"Ctrl-Enter": doEvalFn(cm)});
+});
