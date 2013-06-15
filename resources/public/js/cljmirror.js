aa1dc8f17554ad9e1b4d60562245225673940be6
Martin Trojer <martin.trojer@gmail.com>
2013-06-15 19:37:21 +0100

    comments in js

--- resources/public/js/cljmirror.js
+++ resources/public/js/cljmirror.js
@@ -1,9 +1,13 @@
+// contains [line, widget-obj] entries.
+// sorted by line (thus 'last' widget at end)
 var widgets = [];
 
-function removeWidgets(line) {
+function removeWidgetsBelow(line) {
 	while(widgets.length > 0) {
 		var w = widgets[widgets.length - 1];
 		// TODO -- this doesn't really work. cm.getLineNumber(w) would be better
+		// since you can re-edit 'above' the widgets and thus the line
+		// in widget is wrong.
 		if (w[0] >= line) {
 			w[1].clear();
 			widgets.pop();
@@ -19,16 +23,19 @@ function evalResultFn(cm, line) {
 			var txt = document.createTextNode("Err " + data.err);
 			msg.className = "eval-error";
 		}
+		// a widget can only be one line, thus this is a bit broken
 		else if (data.out != null && data.out != "") {
 			var txt = document.createTextNode(data.out);
 			msg.className = "eval-out";
 		}
+		// .. you also want widgets for both out and result when available
 		else {
 			var txt = document.createTextNode(" ;; " + data.result);
 			msg.className = "eval-result";
 		}
 		msg.appendChild(txt);
 		var cnt = cm.lineCount()-1
+		// add line to doc if we evaling on the last line
 		if (cnt <= line) cm.setValue(cm.getValue()+"\n");
 		widgets.push([line, cm.addLineWidget(line, msg, {})]);
 		widgets.sort(function(a,b) {a[0] - b[0]});
@@ -40,10 +47,11 @@ function doEvalFn(cm) {
 	return function() {
 		var line = cm.getCursor().line
 		var top = 0
-		removeWidgets(line);
+		removeWidgetsBelow(line);
 		if (widgets.length > 0)
 			top = widgets[widgets.length - 1][0]
 
+		// eval all code between current line and last eval (i.e. lastt 'widget')
 		var code = "(do " + cm.getRange({line: top}, {line: line}) + ")";
 
 		$.post("/eval", {code: code}, evalResultFn(cm, line));
@@ -53,7 +61,6 @@ function doEvalFn(cm) {
 // ---
 
 $(document).ready(function() {
-	var widgets = []
 	var cm = CodeMirror(
 		document.getElementById('console'),
 		{value :
